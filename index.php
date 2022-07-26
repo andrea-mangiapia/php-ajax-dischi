e6e09c8153ba1e3f0c88c25f8d822aae17d927d6
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-07-26 15:35:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+include __DIR__ . '/$database.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <img src="img/logo-small.svg" alt="Logo">
+    </header>
+    <main>
+        <section>
+            
+            <!-- Album List -->
+            <div class="list-disc">
+                <div class="single-card">
+                    <div class="img-card">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" :alt="">
+                    </div>
+                    <div class="title-card">
+                        <h2>
+                            Title
+                        </h2>
+                        <div class="author-card">
+                            Author
+                        </div>
+                        <div class="year-card">
+                            Date
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+
+    </main>
+</body>
+</html>
